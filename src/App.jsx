5bafd402fcafc442f4f1e0db0facf8c228822fff
southyd12 <southd12@gmail.com>
2023-03-02 12:50:58 +0000

    added MUI and theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { ThemeProvider } from "@mui/system";
+import theme from './theme/theme';
+
+import Layout from './components/Layout';
+
 import List from './pages/List';
 import Add from './pages/Add';
 import Update from './pages/Update';
 import NotFound from './pages/NotFound';
 
-import Layout from './components/Layout';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
 
   return (
     <Router>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<List />}/>
@@ -20,6 +32,8 @@ function App() {
           <Route path="*" element={<NotFound />}/>
         </Route>
       </Routes>
+    </ThemeProvider>
+      
     </Router>
   )
 }
