18836af81ac4760f56a3ca4a868480a4e90ab3cc
southyd12 <southd12@gmail.com>
2023-03-03 14:33:13 +0000

    Forms added

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Typography from '@mui/material/Typography';
+import CarForm from '../components/forms/CarForm';
 
 function Update() {
   const {id} = useParams();
   return (
     <>
-      <div>Update {id}</div>
+      <Typography
+         variant="h2"
+         component="h1"      
+        >
+        Update Car              
+      </Typography>
+      <CarForm />
     </>
   )
 }
